54971b60908d2706fc08c69a08f9804ff854d089
Bryn Bennett <bryn.t.bennett@gmail.com>
2017-07-14 17:52:58 +1200

    tidy chips

--- src/app/view-issues/view-issues.component.ts
+++ src/app/view-issues/view-issues.component.ts
@@ -18,7 +18,7 @@ export class ViewIssuesComponent implements OnInit {
   }
   Allbreakages: BreakageInfo[];
   breakages: BreakageInfo[];
-  sortList = ["Newest", "Oldest","Most Important","Least Important"];
+  sortList = ["Newest", "Oldest", "Most Important", "Least Important"];
   filterList = ["1", "2", "3", "4", "RIB"];
   appliedFilters = [];
 
@@ -30,20 +30,19 @@ export class ViewIssuesComponent implements OnInit {
       this.appliedFilters.push(key);
     }
 
-    if(this.appliedFilters.length == 0){
+    if (this.appliedFilters.length == 0) {
       this.breakages = this.breakageService.items;
       return;
     }
 
-      this.breakages = this.breakageService.items.filter(
-        item => {
+    this.breakages = this.breakageService.items.filter(
+      item => {
         return this.appliedFilters.some(
-          filter =>
-          {
-          if(item.boatID == filter){
-            return true;
-          }
-        });
+          filter => {
+            if (item.boatID == filter) {
+              return true;
+            }
+          });
       });
 
   }
