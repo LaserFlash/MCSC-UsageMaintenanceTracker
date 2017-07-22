e1cf0a5f8fd98140c9df29f2a1a399136fcc6265
Bryn Bennett <bryn.t.bennett@gmail.com>
2017-07-23 06:07:17 +1200

    generated safety-procedures component

---
+++ src/app/safety-procedures/safety-procedures.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-safety-procedures',
+  templateUrl: './safety-procedures.component.html',
+  styleUrls: ['./safety-procedures.component.css']
+})
+export class SafetyProceduresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
