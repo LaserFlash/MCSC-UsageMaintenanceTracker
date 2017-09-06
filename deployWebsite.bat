bf83941a1aea435c4842b2ae83931b3c39f07f08
Bryn Bennett <bryn.t.bennett@gmail.com>
2017-09-06 14:53:20 +1200

    serviceworker update

--- deployWebsite.bat
+++ deployWebsite.bat
@@ -1,3 +1,4 @@
 call rm -R dist
-call ng build --prod --base-href "https://laserflash.github.io/U23-Reports/"
+call ng build --prod --aot --base-href "https://laserflash.github.io/U23-Reports/"
+call npm run precache
 call ngh
