1cd4dea6c0783c2a0cfb3feaad3e48fe1b3ee8b8
Bryn Bennett <bryn.t.bennett@gmail.com>
2017-11-13 14:18:15 +1300

    fix: graph display after navigation

--- src/app/usage-graphs/usage-graphs.component.ts
+++ src/app/usage-graphs/usage-graphs.component.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
-import { BoatUsageService } from '../boat-usage.service'
+import { Component, Input, OnInit } from '@angular/core';
 import { UsageInfo } from '../objects/usageInfo';
 import { Boats, UserFriendlyBoats } from '../Utils/menuNames'
 
+import { BoatUsageService } from '../boat-usage.service'
+
 
 @Component({
-  selector: 'app-usage-graphs',
+  selector: 'usage-graphs',
   templateUrl: './usage-graphs.component.html',
   styleUrls: ['./usage-graphs.component.css']
 })
-export class UsageGraphsComponent {
-  public chartData: any[] = [{ data: [0, 0, 0, 0, 0, 0, 0, 0], label: 'Hours Used' }];
+export class UsageGraphsComponent implements OnInit {
+  @Input() dataLabel: string;
+  @Input() chartTitle: string;
 
-  constructor(private boatUsageService: BoatUsageService) {
-    boatUsageService.items.subscribe(() => {
-      this.chartData = [{ data: this.boatUsageService.usageTimes, label: 'Hours Used' }];
-    })
+  chartData: any[] = [{ data: [0, 0, 0, 0, 0, 0, 0, 0], label: 'Hours Used' }];
+
+  constructor(private usageService: BoatUsageService) {}
+
+  ngOnInit(){
+    this.chartData = [{ data: this.usageService.usageTimes, label:this.dataLabel}];
+    this.usageService.items.subscribe(()=>{
+      this.chartData = [{ data: this.usageService.usageTimes, label:this.dataLabel}];
+    });
   }
 
-  public colors: Array<any> = [{
+  colors: Array<any> = [{
     backgroundColor: 'rgba(66,165,245,1)',
     borderColor: 'rgba(225,10,241)',
     pointBackgroundColor: 'rgba(66,165,245,1)',
@@ -27,14 +34,14 @@ export class UsageGraphsComponent {
     pointHoverBorderColor: 'rgba(66,165,245,1)'
   }];
 
-  public chartOptions: any = {
+  chartOptions: any = {
     scaleShowVerticalLines: false,
     responsive: true,
     maintainAspectRatio: true,
   };
 
   //Only use boat data for boats with a userfriendly name
-  public chartLabels: string[] = UserFriendlyBoats.filter((s, i) => {
+  chartLabels: string[] = UserFriendlyBoats.filter((s, i) => {
     let yes: boolean = false;
     Boats.forEach(j => {
       yes ? true : yes = i == j;
