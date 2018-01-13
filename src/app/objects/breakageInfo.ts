61f91bfd13b67c47f893bc667ec1885dd30e4541
Bryn Bennett <bryn.t.bennett@gmail.com>
2018-01-14 00:26:31 +1300

    improvement: resize image to make immersive card

--- src/app/objects/breakageInfo.ts
+++ src/app/objects/breakageInfo.ts
@@ -1,15 +1,15 @@
 export class BreakageInfo {
   constructor(
-    public name: string,
-    public contact: string,
-    public boatID: number,
-    public importance: number,
-    public part: string,
-    public details: string,
-    public timestampFixed: Date,
-    public timestamp: Date,
-    public id: string,
-    public imageID: string
+    public name: string = null,
+    public contact: string = null,
+    public boatID: number = null,
+    public importance: number = null,
+    public part: string = null,
+    public details: string = null,
+    public timestampFixed: Date = null,
+    public timestamp: Date = null,
+    public id: string = null,
+    public imageID: string = null
   )
   {}
 }
